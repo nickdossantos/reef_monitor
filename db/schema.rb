1f87c885e0882e2d2682af2f8dc20ef3fbb8b2a2
nickdossantos <nickdos32@gmail.com>
2018-12-05 14:58:31 -0500

    added PostgreSQL PG Gem to Gemfile, and set up local connection vars

--- db/schema.rb
+++ db/schema.rb
@@ -12,6 +12,9 @@
 
 ActiveRecord::Schema.define(version: 20181109145122) do
 
+  # These are extensions that must be enabled in order to support this database
+  enable_extension "plpgsql"
+
   create_table "readings", force: :cascade do |t|
     t.integer "sensor_id"
     t.float "value"
