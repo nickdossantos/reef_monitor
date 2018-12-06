e5daeaecb00eb2e14d68c50e4a1be92c6b8250a2
nickdossantos <nickdos32@gmail.com>
2018-12-06 11:10:47 -0500

    added dashboard view, and authentication on all in app pages

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,3 +1,14 @@
 class ApplicationController < ActionController::Base
   protect_from_forgery with: :exception
+  before_action :authenticate_user!
+  before_action :set_user, unless: :devise_controller?
+
+  def after_sign_in_path_for(resource)
+    dashboard_path(resource.id)
+  end
+
+  private
+  def set_user
+    @user = current_user
+  end
 end
