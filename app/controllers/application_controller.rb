786c9f0a0c14f7033196aeff056a67e56b558e2a
nickdossantos <nickdos32@gmail.com>
2018-12-12 10:25:35 -0500

    added authentication layout resource

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -3,6 +3,8 @@ class ApplicationController < ActionController::Base
   before_action :authenticate_user!
   before_action :set_user, unless: :devise_controller?
 
+  layout :layout_by_resource
+
   def after_sign_in_path_for(resource)
     dashboard_path(resource.id)
   end
@@ -11,4 +13,13 @@ class ApplicationController < ActionController::Base
   def set_user
     @user = current_user
   end
+
+  def layout_by_resource
+    if devise_controller?
+      "authentication"
+    else
+      "application"
+    end
+  end
+
 end
