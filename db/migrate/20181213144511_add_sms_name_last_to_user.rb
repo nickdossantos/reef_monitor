662ed282fe50a829b18163ead163ea23f30f83f9
nickdossantos <nickdos32@gmail.com>
2019-01-07 10:22:40 -0500

    fixed visual bug and migration

--- db/migrate/20181213144511_add_sms_name_last_to_user.rb
+++ db/migrate/20181213144511_add_sms_name_last_to_user.rb
@@ -4,5 +4,6 @@ class AddSmsNameLastToUser < ActiveRecord::Migration[5.1]
       add_column :users, :first_name, :string
       add_column :users, :last_name, :string
       add_column :users, :sms_number, :string
+    end
   end
 end
