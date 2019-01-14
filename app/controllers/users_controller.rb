b8d0e685330e9ad4e94dbc87991fa96ebb2940ee
nickdossantos <nickdos32@gmail.com>
2019-01-14 11:58:36 -0500

    added users notification page

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -63,6 +63,10 @@ class UsersController < ApplicationController
     @user = User.friendly.find(params[:user_id])
   end 
 
+  def notifications
+    @user = User.friendly.find(params[:user_id])
+  end
+
   private
     # Use callbacks to share common setup or constraints between actions.
     def set_user
