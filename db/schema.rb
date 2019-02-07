2d7561f1da1bebcbb3fce7f5df76eb7839b61d67
nickdossantos <nickdos32@gmail.com>
2019-02-07 12:41:39 -0500

    added sms/email notification hours and daily or none options to user

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20190124142830) do
+ActiveRecord::Schema.define(version: 20190207171855) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -89,6 +89,10 @@ ActiveRecord::Schema.define(version: 20190124142830) do
     t.string "hash_id"
     t.string "token"
     t.string "api_endpoint"
+    t.integer "email_notification_frequency", default: 0
+    t.integer "sms_notification_frequency", default: 0
+    t.integer "email_notification_hour", default: 0
+    t.integer "sms_notification_hour", default: 0
     t.index ["email"], name: "index_users_on_email", unique: true
     t.index ["hash_id"], name: "index_users_on_hash_id", unique: true
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
