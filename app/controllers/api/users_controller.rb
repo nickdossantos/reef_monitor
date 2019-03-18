37e94d90bdc806581f7f7dd001c4402fe992bbb8
nickdossantos <nickdos32@gmail.com>
2019-03-18 15:37:27 -0400

    updated api responses

--- app/controllers/api/users_controller.rb
+++ app/controllers/api/users_controller.rb
@@ -1,23 +1,21 @@
 class Api::UsersController < ApplicationController
     skip_before_action :verify_authenticity_token
     skip_before_action :authenticate_user!
-    
-    
     def verify_pin_number
         begin 
             pin_number = params[:pin_number]
             api_endpoint = params[:api_endpoint]
-            user = User.find_by(temporary_pin_token: params[:pin_number])
-            user.temporary_pin_token = pin_number
-            user.api_endpoint = api_endpoint
-            user.save
-            # do something with the pin numbers.
-            # Todo also need to generate the pin numbers and associate them with an account. 
-            render json: {status: "SUCCESS", message: 'Your token has been decoded.'}, status: :ok
+            user = User.find_by(temporary_pin_token: pin_number)
+            if user.temporary_pin_token_expiration_date < Time.now.in_time_zone(user.time_zone)
+                render json: {status: "Fail", message: 'Your pin token has expired.'}, status: :ok
+            else
+                user.temporary_pin_token = pin_number
+                user.api_endpoint = api_endpoint
+                user.save
+                render json: {status: "SUCCESS", message: 'Your token has been decoded.'}, status: :ok
+            end
         rescue => e 
             render json: {status: "FAIL", message: e}, status: :ok
         end
-        
     end 
-
 end
