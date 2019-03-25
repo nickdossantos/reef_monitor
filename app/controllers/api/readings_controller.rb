cba81e22233e17ba6dafb7b4eaf1d6dfe8aca395
nickdossantos <nickdos32@gmail.com>
2019-03-25 15:00:37 -0400

    changed form on tank dashboard to take the temperture pin number in its form

--- app/controllers/api/readings_controller.rb
+++ app/controllers/api/readings_controller.rb
@@ -11,13 +11,12 @@ class Api::ReadingsController < ApplicationController
 
             reading = ReadingService.create_reading(user, payload['date'], payload['hour'], payload['minute'], payload['value'], sensor.id, sensor.tank_id)
             if reading.save
-                render json: {status: "SUCCESS", message: 'Your token has been decoded.', data: decoded_data}, status: :ok
+                render json: {status: "SUCCESS", message: 'Your reading has been created.', data: decoded_data}, status: :ok
             else 
                 render json: {status: "FAIL", message: 'Your token has not been decoded.'}, status: :ok
             end 
         rescue => e 
             render json: {status: "FAIL", message: e}, status: :ok
-        end
-        
+        end        
     end 
 end
